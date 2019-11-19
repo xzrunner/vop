c7994dd7137196b017f85667118b989daafb3ef8
zz <xzrunner@gmail.com>
2019-11-20 07:43:07 +0800

    [CHANGED] use hdiop::Node

--- include/vop/Node.h
+++ include/vop/Node.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include "vop/Variable.h"
+#include "vop/VariableType.h"
+
+#include <hdiop/NodeImpl.h>
 
 #include <SM_Vector.h>
 
@@ -9,7 +11,7 @@
 namespace vop
 {
 
-class Node
+class Node : public hdiop::NodeImpl<VariableType>
 {
 public:
     Node(size_t build_in_count = 0);
@@ -18,52 +20,8 @@ public:
 
     virtual void AddInputPorts(size_t num) {}
 
-    struct Port;
-    void SetImports(const std::vector<Port>& imports) { m_imports = imports; }
-    void SetExports(const std::vector<Port>& exports) { m_exports = exports; }
-
-    auto& GetImports() const { return m_imports; }
-    auto& GetExports() const { return m_exports; }
-
-    void SetName(const std::string& name) { m_name = name; }
-    auto& GetName() const { return m_name; }
-
-    void ClearConnections();
-
-public:
-    struct PortAddr
-    {
-        PortAddr() {}
-        PortAddr(const std::shared_ptr<Node>& node, int idx)
-            : node(node), idx(idx) {}
-        PortAddr(const PortAddr& addr)
-            : node(addr.node), idx(addr.idx) {}
-
-        std::weak_ptr<Node> node;
-        int idx = -1;
-    };
-
-    struct Port
-    {
-        Port() {}
-        Port(const Variable& var)
-            : var(var) {}
-
-        Variable var;
-        std::vector<PortAddr> conns;
-    };
-
-protected:
-    std::vector<Port> m_imports, m_exports;
-
-private:
-    std::string m_name;
-
-    RTTR_ENABLE()
+    RTTR_ENABLE(hdiop::NodeImpl<VariableType>)
 
 }; // Node
-
-void make_connecting(const Node::PortAddr& from, const Node::PortAddr& to);
-void disconnect(const Node::PortAddr& from, const Node::PortAddr& to);
 
 }
