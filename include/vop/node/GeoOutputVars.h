bf78e762832ba3ea41e876a252f5b0077cba3332
zz <xzrunner@gmail.com>
2019-11-20 07:44:33 +0800

    [ADDED] calc func

--- include/vop/node/GeoOutputVars.h
+++ include/vop/node/GeoOutputVars.h
@@ -10,6 +10,14 @@ namespace node
 class GeoOutputVars : public Node
 {
 public:
+    enum InPortIdx
+    {
+        IDX_POS = 0,
+        IDX_COLOR,
+        IDX_NORM,
+    };
+
+public:
     GeoOutputVars()
     {
         m_imports = {
@@ -20,6 +28,13 @@ public:
     }
 
     virtual sm::vec3 Calc(size_t idx) const override;
+
+    sm::vec3 GetPos() const;
+    sm::vec3 GetColor() const;
+    sm::vec3 GetNormal() const;
+
+private:
+    sm::vec3 CalcIn(size_t idx) const;
 
     RTTR_ENABLE(Node)
 
--- source/node/GeoOutputVars.cpp
+++ source/node/GeoOutputVars.cpp
@@ -12,5 +12,42 @@ sm::vec3 GeoOutputVars::Calc(size_t idx) const
     return ret;
 }
 
+sm::vec3 GeoOutputVars::GetPos() const
+{
+    return CalcIn(IDX_POS);
+}
+
+sm::vec3 GeoOutputVars::GetColor() const
+{
+    return CalcIn(IDX_COLOR);
+}
+
+sm::vec3 GeoOutputVars::GetNormal() const
+{
+    return CalcIn(IDX_NORM);
+}
+
+sm::vec3 GeoOutputVars::CalcIn(size_t idx) const
+{
+    sm::vec3 ret;
+    ret.MakeInvalid();
+
+    assert(idx >= 0 && idx < m_imports.size());
+    auto& conns = m_imports[idx].conns;
+    if (conns.empty()) {
+        return ret;
+    }
+
+    assert(conns.size() == 1);
+    auto& conn = conns.front();
+    auto in_node = conn.node.lock();
+    if (!in_node) {
+        return ret;
+    }
+
+    assert(in_node->get_type().is_derived_from<Node>());
+    return std::static_pointer_cast<Node>(in_node)->Calc(conn.idx);
+}
+
 }
 }
